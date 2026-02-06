b0e4e22f1176d90ee9ddf0fdf68376806472a602
Elmer Santos <esantos2019254@kinal.edu.gt>
2026-02-06 11:54:37 -0600

    proyecto terminado

--- src/AuthServiceIN6BM.Api/Extensions/ServiceCollectionExtensions.cs
+++ src/AuthServiceIN6BM.Api/Extensions/ServiceCollectionExtensions.cs
@@ -11,12 +11,18 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<ApplicationDbContext>(options => 
+        services.AddDbContext<ApplicationDbContext>(options =>
             options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
                 .UseSnakeCaseNamingConvention());
-        
+
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<IRoleRepository, RoleRepository>();
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IUserManagementService, UserManagementService>();
+        services.AddScoped<IPasswordHashService, PasswordHashService>();
+        services.AddScoped<IJwtTokenService, JwtTokenService>();
+        services.AddScoped<ICloudinaryService, CloudinaryService>();
+        services.AddScoped<IEmailService, EmailService>();
 
         services.AddHealthChecks();
 

665b6b070ec3a8e9fb99cd1ff92b2999a941a66e
Elmer Santos <esantos2019254@kinal.edu.gt>
2026-02-06 08:19:41 -0600

    proyecto .net

---
+++ src/AuthServiceIN6BM.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+using AuthServiceIN6BM.Application.Interfaces;
+using AuthServiceIN6BM.Application.Services;
+using AuthServiceIN6BM.Domain.Interfaces;
+using AuthServiceIN6BM.Persistence.Data;
+using AuthServiceIN6BM.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServiceIN6BM.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
+                .UseSnakeCaseNamingConvention());
+        
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IRoleRepository, RoleRepository>();
+
+        services.AddHealthChecks();
+
+        return services;
+
+    }
+
+    public static IServiceCollection AddApiDocumentation(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+}
---
+++ src/AuthServiceIN6BM.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,15 @@
+using AuthServiceIN6BM.Application.DTOs;
+using AuthServiceIN6BM.Application.DTOs.Email;
+
+namespace AuthServiceIN6BM.Application.Interfaces;
+
+public interface IAuthService
+{
+    Task<RegisterResponseDto> RegisterAsync(RegisterDto registerDto);
+    Task<AuthResponseDto> LoginAsync(LoginDto loginDto);
+    Task<EmailResponseDto> VerifyEmailAsync(VerifyEmailDto verifyEmailDto);
+    Task<EmailResponseDto> ResendVerificationEmailAsync(ResendVerificationDto resendDto);
+    Task<EmailResponseDto> ForgotPasswordAsync(ForgotPasswordDto forgotPassswordDto);
+    Task<EmailResponseDto> ResetPasswordAsync(ResetPasswordDto resetPasswordDto);
+    Task<UserResponseDto> GetUserByIdAsync(string userId);
+}
